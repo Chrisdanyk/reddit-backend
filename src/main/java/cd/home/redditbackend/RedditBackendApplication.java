c81ae323443365d7bf997cf18fff221d380e69e5
chris dany <ckandanda@scopex.be>
2021-09-25 15:13:51 +0200

    initial commit

---
+++ src/main/java/cd/home/redditbackend/RedditBackendApplication.java
@@ -0,0 +1,13 @@
+package cd.home.redditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditBackendApplication.class, args);
+    }
+
+}
