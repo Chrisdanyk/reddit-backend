9525c30c9f5b447d324762acdbba5d5bd26de52a
chris dany <ckandanda@scopex.be>
2021-10-18 11:34:09 +0200

    added comment creation logic

--- src/main/java/cd/home/redditbackend/repository/CommentRepository.java
+++ src/main/java/cd/home/redditbackend/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package cd.home.redditbackend.repository;
 
 import cd.home.redditbackend.model.Comment;
+import cd.home.redditbackend.model.Post;
+import cd.home.redditbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
 }
---
+++ src/main/java/cd/home/redditbackend/service/CommentService.java
@@ -0,0 +1,65 @@
+package cd.home.redditbackend.service;
+
+import cd.home.redditbackend.data.CommentsDto;
+import cd.home.redditbackend.exceptions.PostNotFoundException;
+import cd.home.redditbackend.mapper.CommentMapper;
+import cd.home.redditbackend.model.Comment;
+import cd.home.redditbackend.model.NotificationEmail;
+import cd.home.redditbackend.model.Post;
+import cd.home.redditbackend.model.User;
+import cd.home.redditbackend.repository.CommentRepository;
+import cd.home.redditbackend.repository.PostRepository;
+import cd.home.redditbackend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class CommentService {
+    private static final String POST_URL = "";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto commentsDto) {
+        log.warn("p-id ->" + commentsDto.getPostId());
+        Post post = postRepository.findById(commentsDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+        Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(authService.getCurrentUser() + " posted a comment on your post." + comment.getPost().getUrl());
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + " Commented on your post", user.getEmail(), message));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto).collect(toList());
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+}
